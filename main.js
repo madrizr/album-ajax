3cf55a02d7655cb88b2737ef343e7c3b12681143
robert <test@test.com>
2021-05-06 11:41:50 -0430

    album ajax

---
+++ main.js
@@ -0,0 +1,31 @@
+document.querySelector('#boton').addEventListener('click', traerDatos());
+
+function traerDatos(){
+	console.log("ejecutado")
+
+	const xhttp = new XMLHttpRequest();
+
+	xhttp.open('GET', 'cds.json', true);
+
+	xhttp.send();
+
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			//console.log(this.responseText);
+			let datos = JSON.parse(this.responseText);
+			console.log(datos);
+			let res = document.querySelector('#res');
+			res.innerHTML = '';
+
+			for(let item of datos){
+				// console.log(item.titulo)
+				res.innerHTML += `
+					<tr>
+						<td>${item.titulo}</td>
+						<td>${item.Artista}</td>
+					</tr>
+				`
+			}
+		}
+	}
+}
